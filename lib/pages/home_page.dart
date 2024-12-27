051b2722518927e50587f6d6ed7e162a816a9dd2
Anurag <uanurag216@gmail.com>
2024-12-27 22:54:42 +0530

    added commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: ListView(
+        scrollDirection: Axis.vertical,
+        children: [
+
+          //main
+           Container(
+             height: 500,
+             width: double.maxFinite,
+
+           ),
+
+          //skills
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+          ),
+
+          //projects
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+          ),
+
+          //Contact
+          Container(
+            height: 500,
+            width: double.maxFinite,
+            color: Colors.blueGrey,
+          ),
+
+        ],
+      ),
+    );
+  }
+}
