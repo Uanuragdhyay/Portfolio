6f7651e5306c53b45040714d9f82e42106d8d4d7
Anurag <uanurag216@gmail.com>
2024-12-29 13:46:38 +0530

    added commit

---
+++ lib/constants/colors.dart
@@ -0,0 +1,13 @@
+import 'dart:ui';
+class CustomColor{
+  static const Color scaffoldBg = Color(0xff252734);
+  static const Color bgLight1 = Color(0xff252734);
+  static const Color bgLight2 = Color(0xff252734);
+  static const Color textFieldBg = Color(0xff252734);
+  static const Color hintDark = Color(0xff252734);
+  static const Color yellowSecondary = Color(0xff252734);
+  static const Color yellowPrimary = Color(0xff252734);
+  static const Color whitePrimary= Color(0xff252734);
+  static const Color whiteSecondary= Color(0xff252734);
+
+}
